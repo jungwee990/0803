7915534e6848c2fd60df93e7b5c921537e49dadd
jungwee990 <wjddnl100@gmail.com>
2021-08-04 11:29:35 +0900

    클래스1

--- Function2.py
+++ Function2.py
@@ -14,3 +14,54 @@ def func2(a):
     return x+a
 #호출
 print(func2(1))
+
+#불변형식을 함수 내부에서 읽기 + 쓰기
+# g = 1
+# def testScope(a):
+#      global g
+#     g = 2
+#     return g+a
+
+# #호출
+# print(testScope(1))
+# print("전역변수 g:",g)
+
+#기본값을 세팅
+def times(a=10, b=20):
+    return a*b
+
+#호출
+print(times())
+print(times(a=5))
+print(times(3,4))
+
+#키워드 인자(파라메터명을 명시)
+def connectURI(server, port):
+    strURL = "http://" + server + ":" + port
+    return strURL
+
+#호출
+print(connectURI("bitCamp", "80"))
+print(connectURI(port = "80",server ="bitcamp"))
+
+#가변인자(다양한 개수를 소화)
+def union(*ar):
+    #지역변수(리스트)
+    result = []
+    #HAM(0) | EGG(1)
+    for item in ar:
+        #H(0) | A(1) | M(2)
+        for x in item:
+            if x not in result:
+                result.append(x)
+    return result
+
+#호출 
+print(union("HAM", "EGG"))
+print(union("HAM", "EGG", "SPAM"))
+
+
+
+
+
+
